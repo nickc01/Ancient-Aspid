22ee8b684aa530b313554140103599d3d0d41f4d
nickc01 <nickc01@outlook.com>
2024-04-21 12:56:21 -0500

    Fixed Pale Court music issues. Fixed the lag associated with loading music files

--- Assets/AA_AlternativeMusicController.cs
+++ Assets/AA_AlternativeMusicController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using WeaverCore;
 using static AncientAspidMusicController;
@@ -39,6 +40,35 @@ public class AA_AlternativeMusicController : MonoBehaviour
         Music.ApplyMusicSnapshot(Music.SnapshotType.Normal, 0f, 0f);
         WeaverAudio.PlayAtPoint(introClip, Player.Player1.transform.position, channel: WeaverCore.Enums.AudioChannel.Music);
         StartCoroutine(PlayRoutine(startPhase));
+        StartCoroutine(PreloaderRoutine());
+    }
+
+    //[NonSerialized]
+    //List<AudioPlayer> preloads = new List<AudioPlayer>();
+
+    IEnumerator PreloaderRoutine()
+    {
+        List<MusicChannels> channels = new List<MusicChannels>
+        {
+            MusicChannels.Main,
+            MusicChannels.MainAlt,
+            MusicChannels.Action,
+            MusicChannels.Sub,
+            MusicChannels.Tension,
+            MusicChannels.Extra
+        };
+
+        float duration = introDuration * 0.75f;
+
+        foreach (var channel in channels.Select(alternativeMusicCue.GetChannelInfo))
+        {
+            if (channel != null && channel.Clip != null)
+            {
+                //preloads.Add(WeaverAudio.PlayAtPoint(channel.Clip, Player.Player1.transform.position, 0.002f, WeaverCore.Enums.AudioChannel.Music));
+                channel.Clip.LoadAudioData();
+                yield return new WaitForSecondsRealtime(duration / channels.Count);
+            }
+        }
     }
 
     IEnumerator PlayRoutine(MusicPhase startPhase)
@@ -47,6 +77,11 @@ public class AA_AlternativeMusicController : MonoBehaviour
         Music.PlayMusicCue(alternativeMusicCue, 0f, 0f, false);
         var index = phases.IndexOf(startPhase);
         Music.ApplyMusicSnapshot(phaseSnapshots[index], 0f, 0);
+
+        /*foreach (var preload in preloads)
+        {
+            preload.Delete();
+        }*/
     }
 
     public void TransitionToPhase(MusicPhase phase)
--- Assets/Scripts/AncientAspidMod.cs
+++ Assets/Scripts/AncientAspidMod.cs
@@ -17,7 +17,7 @@ public class AncientAspidMod : WeaverMod
 
     public override string GetVersion()
     {
-        return "1.0.0.5";
+        return "1.0.0.6";
     }
 
     public override void Initialize()
